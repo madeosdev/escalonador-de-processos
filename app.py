375181b4e0e24524baa5b6c21037e683515b96e7
madeosdev <matheosaugusto@proton.me>
2026-03-10 12:07:24 -0300

    Modificações na interface do sistema.

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import os
 from time import sleep
 import calculus
+import numpy as np
 
-print('Placeholder')
+print('-'*40)
+print('Escalonador de Processos by MadeosDev!')
+print('-'*40)
+array_processos = np.array([])
+array_espera = np.array([])
+lista_inicios = list()
+
+while True:
+    num_processos = int(input('Quantos processos você quer calcular?'))
+    for i in range(0, num_processos):
+        inicio = int(input(f'Ponto de início do Processo {i+1}?'))
+        lista_inicios.append(inicio)
+        tmp = int(f'Qual é o tempo de duração do Processo {i+1}?')
+        tme = int(input(f'Qual é o tempo de espera do Processo {i+1}'))
+        
